97988edb7e4e1abf528d95e3a6eb2d3e5c5ea3be
Nishta03 <nishtamayekar0@gmail.com>
2026-06-01 22:54:42 +0530

    Update basicClaculator.py

--- basicClaculator.py
+++ basicClaculator.py
@@ -1,26 +1,27 @@
-def add(a,b):
-    return a + b
+while True:
+    def add(a,b):
+        return a + b
 
-def sub(a,b):
-    return a - b
+    def sub(a,b):
+        return a - b
 
-def mul(a,b):
-    return a * b
+    def mul(a,b):
+        return a * b
 
-def div(a,b):
-    return a / b
+    def div(a,b):
+        return a / b
 
-a = float(input("Enter the First Number: "))
-b = float(input("Enter the Second Number:"))
-op = input("Enter Operation: (+,-,*,/): ")
+    a = float(input("Enter the First Number: "))
+    b = float(input("Enter the Second Number:"))
+    op = input("Enter Operation: (+,-,*,/): ")
 
-if op == '+':
-    print(add(a,b))
-elif op == '-':
-    print(sub(a,b))
-elif op == '*':
-    print(mul(a,b))
-elif op == '/':
-    print(div(a,b))
-else:
-    print("Invalid Operation ")
+    if op == '+':
+        print(add(a,b))
+    elif op == '-':
+        print(sub(a,b))
+    elif op == 'x':
+        print(mul(a,b))
+    elif op == '/':
+        print(div(a,b))
+    else:
+        print("Invalid Operation ")
